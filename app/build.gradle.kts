892ab076bb882e7b0418896b95d2f8fed23aed0e
IbadDev5 <m.ibadhassan5@gmail.com>
2025-01-12 14:22:45 +0500

    Signup Form

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.learnwith.hiskytechnologies"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.learnwith.hiskytechnologies"
